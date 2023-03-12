4e3f41e8a6ade06c726c8d3c4ca7e2a5e85c1322
Michael Primavera <pastasauce59@gmail.com>
2023-03-11 21:05:50 -0500

    changed Screen background color and gave it a title, created a snake using a for loop consiting of 3 white square blocks next to each other 20 pixel spaces apart.

--- main.py
+++ main.py
@@ -2,5 +2,19 @@ from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Classic Nokia 3310 Snake Game - In Python')
+
+xaxis = 0
+for _ in range(0,3):
+    new_snake = Turtle('square')
+    new_snake.color('white')
+    new_snake.goto(x=xaxis, y=0)
+    xaxis -= 20
+
+    
+
+
+
 
 screen.exitonclick()
