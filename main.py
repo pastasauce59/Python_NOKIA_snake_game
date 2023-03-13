5a7f16caf2d7ea6a6218211267f64a0fdb966c1c
Michael Primavera <pastasauce59@gmail.com>
2023-03-12 20:30:13 -0400

    Extracted code to make snake segments and created a Snake Class in a separate module, imported into main.py.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,28 +8,13 @@ screen.bgcolor('black')
 screen.title('Classic Nokia 3310 Snake Game - In Python')
 screen.tracer(0)
 
-snake_segments = []
-
-xaxis = 0
-for _ in range(0,3):
-    new_snake = Turtle('square')
-    new_snake.color('white')
-    new_snake.penup()
-    new_snake.goto(x=xaxis, y=0)
-    xaxis -= 20
-    snake_segments.append(new_snake)
-
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    # for snake_num in range(start= 2, stop= 0, step= -1):
-    for snake_num in range(len(snake_segments) - 1, 0, -1):
-        new_x = snake_segments[snake_num - 1].xcor()
-        new_y = snake_segments[snake_num - 1].ycor()
-        snake_segments[snake_num].goto(new_x, new_y)
-    snake_segments[0].forward(20)
-    snake_segments[0].left(30)
+    snake.move()
+
 screen.exitonclick()
